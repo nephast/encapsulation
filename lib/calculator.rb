108d90eb3dc71fb9b4a839175d6b018bde3455eb
Stéphan Gaud <stephgoeseast@gmail.com>
2016-08-17 11:08:48 +0100

    create calculator and display classes.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,26 @@
-def add(x,y)
-  x + y
+class Calculator
+
+  def initialize
+    @output = Display.new
+  end
+
+  def add(x, y)
+    output(x + y)
+  end
+
+  def subtract(x, y)
+    output(x - y)
+  end
+
+private
+
+  def output(number)
+    @output.message(number)
+  end
 end
 
-def subtract(x,y)
-  x - y
+class Display
+  def message(x)
+    "Your result is #{x}"
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,21 +1,23 @@
 require 'calculator'
 
-describe "calculator" do
+describe Calculator do
 
   it "adds two numbers" do
-    expect(add(1,1)).to eq(2)
+    expect(subject.add(1,1)).to eq("Your result is 2")
   end
 
   it 'add 2 + 2' do
-    expect(add(2,2)).to eq(4)
+    expect(subject.add(2,2)).to eq("Your result is 4")
   end
 
   it 'subtracts 1-1' do
-    expect(subtract(1,1)).to eq(0)
+    expect(subject.subtract(1,1)).to eq("Your result is 0")
   end
 
   it 'subtracts 2-1' do
-    expect(subtract(2,1)).to eq(1)
+    expect(subject.subtract(2,1)).to eq("Your result is 1")
   end
-  
+
+
+
 end
